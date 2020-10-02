7ab9a7d1e96fe62b58e0a5fd4ca702ff117bf3f2
as5845131421 <a1091372@gmail.com>
2020-10-02 17:54:05 +0800

    init commit

---
+++ precache-manifest.39a2961a7f58453434d3f8c4616ef45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e70b67cdac743458e6c6fef501d7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d000df1ba8d041238b16",
+    "url": "/static/css/main.63064a23.chunk.css"
+  },
+  {
+    "revision": "45bd4e6fc57aacd444ca",
+    "url": "/static/js/2.f8595686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8595686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d000df1ba8d041238b16",
+    "url": "/static/js/main.6beb5c73.chunk.js"
+  },
+  {
+    "revision": "46499f15f1155d20ea53",
+    "url": "/static/js/runtime-main.93dfc2dd.js"
+  }
+]);
